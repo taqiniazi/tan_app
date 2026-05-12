434a1161e9f93a909d8020d00643aa3290a81b19
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-05-13 01:04:57 +0500

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -14,7 +14,7 @@ class ApiService {
 
   static const String _lastActivityKey = 'last_activity';
   static const int _inactivityTimeoutHours = 48;
-  static const String _baseUrl = 'https://tannetwork.online/tan-laravel/api';
+  static const String _baseUrl = 'https://tannetwork.online/tan-laravel/public/index.php/api';
   String get baseUrl => _baseUrl;
 
   ApiService() {
